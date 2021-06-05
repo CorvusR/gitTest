28a4d1110bd1a9b64cdacd58995a7da7061825b6
XIEHE-IT03 <1@corvus.com>
2021-06-05 10:19:09 +0800

    test1

--- src/main/java/com/corvus/test1/TestGit.java
+++ src/main/java/com/corvus/test1/TestGit.java
@@ -3,5 +3,6 @@ package com.corvus.test1;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("main test1");
+        System.out.println("push test");
     }
 }
