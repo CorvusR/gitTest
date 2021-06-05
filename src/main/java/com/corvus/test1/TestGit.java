b26bba0592c6dfd8745dd8f3fe9349156a4ec463
CorvusR <75421800+CorvusR@users.noreply.github.com>
2021-06-05 10:20:47 +0800

    Update TestGit.java

--- src/main/java/com/corvus/test1/TestGit.java
+++ src/main/java/com/corvus/test1/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("main test1");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
